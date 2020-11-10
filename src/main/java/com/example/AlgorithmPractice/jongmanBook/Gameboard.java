6003ab10a61cab405c651c1b061904da317e2d46
N19071 <hj.min1031@interpark.com>
2020-11-10 12:47:20 +0900

    게임판덮기 예제문제는 통과

--- src/main/java/com/example/AlgorithmPractice/jongmanBook/Gameboard.java
+++ src/main/java/com/example/AlgorithmPractice/jongmanBook/Gameboard.java
@@ -14,8 +14,7 @@ public class Gameboard {
     static int answer = 0;
 
     public static void main(String args[]) throws Exception {
-
-        String file = "C:\\Users\\hjmin\\Downloads\\gameboard.txt";
+        String file = "C:\\Users\\민희정\\Downloads\\gameboard.txt";
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
 
         /* 문제 읽기 */
@@ -40,48 +39,43 @@ public class Gameboard {
                     }
                 }
             }
-            
+
             /* 로직 시작 */
             if(whiteSpaceCnt%3 != 0){
                 System.out.println(noOfWaysToCover);
             } else {
-
-                for(int x=0; x<height; x++){
-                    for(int y=0; y<width; y++){
-                       int answer = coverChk(x, y);
-                    }
-                }
+                coverChk();
                 System.out.println(answer);
             }
             answer = 0;
         }
-
-
     }
 
-    private static int[][][] coverType = new int[][][]{ {
-                                                            {0,0},{1,0},{0,1}
-                                                        },
+    private static int[][][] coverType = new int[][][]{ {{0,0},{1,0},{0,1}},
                                                         {{0,0},{0,1},{1,1}},
                                                         {{0,0},{1,0},{1,1}},
                                                         {{0,0},{1,0},{1,-1}}
                                                 };
 
-    public static int coverChk(int x, int y){
-        int answer = 0;
-        /*기저베이스 : 다 덮였는지 확인*/
-        int cntFalse = 0;
-        for(boolean[] row : board){
-            for(boolean b : row){
-                if(!b) {
-                    ++cntFalse;
-                    break;
+    public static void coverChk(){
+        boolean isAllCovered = true; /*기저베이스 : 다 덮였는지 확인*/
+        int x = 0;
+        int y = 0;
+
+        Loop :
+        for(int h=0; h<height; h++){
+            for(int w=0; w<width; w++){
+                if(!board[h][w]) {
+                    x = h;
+                    y = w;
+                    isAllCovered = false;
+                    break Loop;
                 }
             }
         }
 
-        if(cntFalse == 0) {
-            return 1;
+        if(isAllCovered) {
+            ++answer;
         }
 
         /* 반복의 조각 */
@@ -109,68 +103,11 @@ public class Gameboard {
                 board[x][y] = true;
                 board[x+(coverType[i][1][0])][y+(coverType[i][1][1])] = true;
                 board[x+(coverType[i][2][0])][y+(coverType[i][2][1])] = true;
-                answer += coverChk(x, y+1);
-                board[i][0] = board[i][1] = board[i][2] = false;
-            } else {
-                break;
+                coverChk();
+                board[x][y] = false;
+                board[x+(coverType[i][1][0])][y+(coverType[i][1][1])] = false;
+                board[x+(coverType[i][2][0])][y+(coverType[i][2][1])] = false;
             }
         }
-
-        return 0;
     }
-
-
-
-//    public static boolean checkShapeIfCanCover(int x, int y, int shape) {
-//        for( int i = 0; i < 3; i++){
-//            int newX = x + coverType[shape][i][0];
-//            int newY = y + coverType[shape][i][1];
-//            if((newX < 0 || newX > height-1) || (newY < 0 || newY > width-1)){
-//                if(!board[newX][newY]){
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-
-
-    public static int[] checkRange(int x, int y){
-
-        int[] pos = new int[2];
-        int cnt = 1;
-        Loop :
-        for(int i=0; i < coverType.length; i++){
-            for(int j=1; j < coverType[i].length; j++){
-                int newX = x + coverType[i][j][0];
-                int newY = y + coverType[i][j][1];
-
-                /* 판을 넘어가는지, 이미 덮여 있거나, 검은 판이지 확인 */
-                if((newX < 0 || newX > height-1) || (newY < 0 || newY > width-1)){
-                    break;
-                } else {
-                    if(!board[newX][newY]){
-                        ++cnt;
-                        if(cnt == 3) {
-                            board[x][y] = true;
-                            board[x+coverType[i][j-1][0]][y+coverType[i][j-1][1]] = true;
-                            board[newX][newY] = true;
-                            pos[0] = i;
-                            pos[1] = j;
-                            break Loop;
-//                            coverChk();
-//                            board[x][y] = false;
-//                            board[x+coverType[i][j-1][0]][y+coverType[i][j-1][1]] = false;
-//                            board[newX][newY] = false;
-                        }
-                    }
-                }
-            }
-            cnt = 1;
-        }
-
-        return pos;
-    }
-
-
 }
